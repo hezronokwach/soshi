dcaaa65ac2ffe69411877091e644cc3049c77d8e
Stephen Kisengese <skisengese@outlook.com>
2025-06-21 19:50:07 +0300

    Chore: restore file changes

--- src/app/groups/page.js
+++ src/app/groups/page.js
@@ -1,10 +1,10 @@
 "use client";
+import GroupComponent from '@/components/groups/GroupComponent';
 
 export default function GroupsPage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Groups</h1>
-      <p>Groups page coming soon...</p>
+      <GroupComponent />
     </div>
   );
 }
