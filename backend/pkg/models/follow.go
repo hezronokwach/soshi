65ee222f9fc70ba6a0cfb624f79a269c44104c20
bshisia <shisiabrian7@gmail.com>
2025-07-10 14:05:51 +0300

    fix: remove unnecessary blank lines in AcceptFollowRequest function

--- backend/pkg/models/follow.go
+++ backend/pkg/models/follow.go
@@ -361,6 +361,8 @@ func AcceptFollowRequest(db *sql.DB, followerId int, followingId int) error {
 	return RespondToFollowRequest(db, followerId, followingId, "accepted")
 }
 
+
+
 // AcceptMessageRequest sets the follow status to accepted (accepts a message request)
 func AcceptMessageRequest(db *sql.DB, userID, requesterID int) error {
 	// userID is the recipient, requesterID is the sender of the message request
